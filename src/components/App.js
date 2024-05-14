53302de1eaa704eb90d0f0c9d37617bc8c0116e3
jessetfb <langatjesse@gmail.com>
2024-05-14 15:27:19 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,37 @@
-import React from "react";
+import { React, useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
-
 function App() {
+  const [tasksToDisplay, setTasksToDisplay] = useState(TASKS);
+  function handleCategoryClick(category) {
+    console.log(category);
+    setTasksToDisplay(TASKS.filter(task => {
+      if (category === "All") return true;
+      return task.category === category;
+    }
+    ));
+  }
+  function handleDeleteClick(text) {
+    console.log("delete", text);
+    setTasksToDisplay(tasksToDisplay.filter(task => !(task.text === text)));
+
+  }
+  function onTaskFormSubmit (e){
+    console.log(e)
+    setTasksToDisplay([...tasksToDisplay, e]);
+  }
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
+      <CategoryFilter categories={CATEGORIES} handleCategoryClick={handleCategoryClick} />
       <NewTaskForm />
-      <TaskList />
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={onTaskFormSubmit }  />
+      <TaskList tasks={tasksToDisplay} handleDeleteClick={handleDeleteClick} />
     </div>
   );
 }
-
 export default App;
